e2cb2ac7f3e1a224cb81acc9a992cc047eee57b0
VaishnaviSubnis <vaishnavisubnis83@gmail.com>
2026-07-07 22:05:56 +0530

    degree celsius

---
+++ c15.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    float c,f;
+    printf("The value of celsius:");
+    scanf("%f",&c);
+    f=(9.0/5.0)*c+32;
+    printf("The value of fahrenheit:%f",f);
+    return 0;
+
+}
