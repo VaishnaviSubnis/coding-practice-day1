fb112aa6f99fe48f2ae9f7e0088e22f5cd807575
VaishnaviSubnis <vaishnavisubnis83@gmail.com>
2026-07-09 23:28:52 +0530

    if else practice

---
+++ c19.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int a;
+printf("Enter the value of number:");
+scanf("%d",&a);
+if (a>=10)
+printf("You are a free elf");
+else
+printf("You are dobby");
+return 0;
+
+}
