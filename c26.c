e9a6609d2582898cef5c6744980fa82bcddfc55f
VaishnaviSubnis <vaishnavisubnis83@gmail.com>
2026-07-10 20:45:05 +0530

    greatest of three

---
+++ c26.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int  a,b,c;
+printf("Enter the numbers:");
+scanf("%d %d %d",&a,&b,&c);
+if(a>b && a>c)
+{
+printf("%d is greatest of all.",a);
+}
+else if(b>a && b>c)
+{
+printf("%d is greatest of all.",b);
+}
+else
+{
+printf("%d is greatest of all.",c);
+}
+return 0;
+}
